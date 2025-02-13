2d7c2c8f39961cd4c15917cf0ff0dc367ac0ac57
Chia1104 <yuyuchia7423@gmail.com>
2025-02-13 14:13:33 +0800

    fix: fix the build issue

--- src/themes/fonts.ts
+++ src/themes/fonts.ts
@@ -1,5 +1,5 @@
 import { Michroma, Noto_Sans } from 'next/font/google';
 
-export const michroma = Michroma({ weight: '400' });
+export const michroma = Michroma({ weight: '400', subsets: ['latin'] });
 
-export const noto_sans = Noto_Sans();
+export const noto_sans = Noto_Sans({ subsets: ['latin'] });
